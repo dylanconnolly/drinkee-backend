a31a1d6008d4b5f3f671a44d36c2083e89c42d69
Dylan Connolly <dylanconnolly145@gmail.com>
2020-02-21 15:34:34 -0700

    Create ingredient model and ingredient data formatter classes

---
+++ app/models/ingredient_data_formatter.rb
@@ -0,0 +1,13 @@
+class IngredientDataFormatter
+  attr_reader :hash_data
+
+  def initialize(ingredient_api_data)
+    @hash_data = {
+      name: ingredient_api_data["strIngredient"],
+      description: ingredient_api_data["strDescription"],
+      # type: ingredient_api_data["strType"],
+      alcohol: ingredient_api_data["strAlcohol"],
+      abv: ingredient_api_data["strABV"]
+    }
+  end
+end
