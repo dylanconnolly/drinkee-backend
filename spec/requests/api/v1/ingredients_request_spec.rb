8960c3625b4b095ddb386eded3448cc006646f02
Dylan Connolly <dylanconnolly145@gmail.com>
2020-02-22 14:57:19 -0700

    Add expectation to test

--- spec/requests/api/v1/ingredients_request_spec.rb
+++ spec/requests/api/v1/ingredients_request_spec.rb
@@ -2,14 +2,14 @@ require './spec/spec_helper'
 
 describe 'ingredients request' do
   it 'returns all of the ingredients in the database' do
-    # create_list(:ingredient, 10)
-    #
-    get '/ingredients'
+    create_list(:ingredient, 10)
 
+    get '/ingredients'
 
     expect(last_response).to be_successful
 
     ingredients = JSON.parse(last_response.body)
-    
+
+    expect(ingredients["data"].count).to eq(10)
   end
 end
