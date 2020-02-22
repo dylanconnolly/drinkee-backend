42e841f00b8b307567fef6670b93ed98a99203af
Dylan Connolly <dylanconnolly145@gmail.com>
2020-02-22 14:10:12 -0700

    Add ingredients api endpoint for testing purposes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,18 @@
 require 'sinatra'
 
+
 class ApplicationController < Sinatra::Base
-  # set :method_override, true
+  set :method_override, true
   # set :root, APP_ROOT
 
   get '/' do
     "HEY THERE THIS IS WORKING NOW"
+  end
+
+  get '/ingredients' do
+    content_type  :json
+    
+    Ingredient.all.to_json
   end
 end
 
