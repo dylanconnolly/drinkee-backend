86be9a173457663b922ccea53161a73f9d7882ac
Dylan Connolly <dylanconnolly145@gmail.com>
2020-02-24 13:25:08 -0700

    Update route to be api/v1/ingredients

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,21 +11,21 @@ class ApplicationController < Sinatra::Base
     "HEY THERE THIS IS WORKING NOW"
   end
 
-  # get '/ingredients' do
-  #   content_type  :json
-  #
-  #   ingredients = Ingredient.all
-  #
-  #   IngredientSerializer.new(ingredients).to_json
-  # end
-  #
-  # get '/ingredients/:id' do
-  #   content_type :json
-  #
-  #   ingredient = Ingredient.find(params[:id])
-  #
-  #   IngredientSerializer.new(ingredient).to_json
-  # end
+  get '/api/v1/ingredients' do
+    content_type  :json
+
+    ingredients = Ingredient.all
+
+    IngredientSerializer.new(ingredients).to_json
+  end
+
+  get '/api/v1/ingredients/:id' do
+    content_type :json
+
+    ingredient = Ingredient.find(params[:id])
+
+    IngredientSerializer.new(ingredient).to_json
+  end
 end
 
 # class ApplicationController < DrinkeeApp
