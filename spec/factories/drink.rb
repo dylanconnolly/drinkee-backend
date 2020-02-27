ca193798053d90f19035ef4c49d2dae87dd51dc4
Dylan Connolly <dylanconnolly145@gmail.com>
2020-02-27 08:23:17 -0700

    Create drink and cabinet factories

---
+++ spec/factories/drink.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :drink do
+    name { }
+    category { "booze" }
+    alcoholic { "yes"}
+    glass { "collins" }
+    instructions { "mix ingredients you dummy" }
+    image { "noimg" }
+  end
+end
