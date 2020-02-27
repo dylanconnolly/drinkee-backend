6a25f63d8031a0be0e2cb67f387581f1307eb102
Dylan Connolly <dylanconnolly145@gmail.com>
2020-02-27 16:14:54 -0700

    Change database.rb to run in development test only

--- config/database.rb
+++ config/database.rb
@@ -1,14 +1,17 @@
-database_name = "drinkee-#{ENV['RACK_ENV']}"
+if ENV['RACK_ENV'] == 'development' || 'test'
 
-db = URI.parse(ENV['DATABASE_URL'] || "postgres://localhost/#{database_name}")
+  database_name = "drinkee-#{ENV['RACK_ENV']}"
 
-# connect ActiveRecord with the current database
-ActiveRecord::Base.establish_connection(
-  :adapter  => 'postgresql',
-  :host     => db.host,
-  # :port     => db.port,
-  :username => db.user,
-  :password => db.password,
-  :database => "#{database_name}",
-  :encoding => 'utf8'
-)
+  db = URI.parse(ENV['DATABASE_URL'] || "postgres://localhost/#{database_name}")
+
+  # connect ActiveRecord with the current database
+  ActiveRecord::Base.establish_connection(
+    :adapter  => 'postgresql',
+    :host     => db.host,
+    # :port     => db.port,
+    :username => db.user,
+    :password => db.password,
+    :database => "#{database_name}",
+    :encoding => 'utf8'
+  )
+end
