b4521de4fb9fee0243a20959a1167a5e751438ef
Dylan Connolly <dylanconnolly145@gmail.com>
2020-02-27 08:23:48 -0700

    Create drink results endpoint and logic to return drinks

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -34,7 +34,9 @@ class ApplicationController < Sinatra::Base
   patch '/api/v1/:user_id/cabinet' do
     cabinet = Cabinet.find_by_user_id(params[:user_id])
     ingredient_ids = JSON.parse(request.body.read)
+
     cabinet.ingredients = []
+
     ingredient_ids.each do |ingredient_id|
       ingredient = Ingredient.find(ingredient_id)
       cabinet.ingredients << ingredient
@@ -48,7 +50,27 @@ class ApplicationController < Sinatra::Base
 
     IngredientSerializer.new(cabinet.ingredients).to_json
   end
+
+  get '/api/v1/:user_id/results' do
+    content_type :json
+
+    cabinet = Cabinet.find_by_user_id(params[:user_id])
+    drinks = []
+    Drink.all.each do |drink|
+      results = drink.ingredients.map do |ingredient|
+        cabinet.ingredients.include?(ingredient)
+      end
+      if results.include?(false)
+      else
+        drinks << drink
+      end
+    end
+    drinks
+
+    DrinkSerializer.new(drinks).to_json
+  end
 end
+
 
 # class ApplicationController < DrinkeeApp
 #
